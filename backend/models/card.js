1d2975fbfa2503f43d07f6ab7a204f1c3fb4fcc5
Kazzzemir <s1ckwell@mail.ru>
2023-10-09 16:27:32 +0300

    nsrg

--- backend/models/card.js
+++ backend/models/card.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
+const ownerSchema = mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+});
+
 const cardSchema = mongoose.Schema({
   name: {
     type: String,
@@ -13,10 +21,7 @@ const cardSchema = mongoose.Schema({
     ref: 'user',
   },
   likes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'user',
-    default: [],
+    ownerSchema,
   }],
   link: {
     type: String,
